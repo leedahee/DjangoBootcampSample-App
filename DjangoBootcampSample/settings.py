75df5aa8f0a6de73b2d35fdde24bc42254b48b1b
dahee <dahee@umich.edu>
2017-05-03 23:30:27 -0400

    add Procfile and requirements.txt

--- DjangoBootcampSample/settings.py
+++ DjangoBootcampSample/settings.py
@@ -124,3 +124,24 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
 STATIC_URL = '/static/'
+
+
+# Add fix for status files for deploy
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/1.10/howto/static-files/
+# The absolute path to the directory where collectstatic will collect static files for deployment.
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+# The URL to use when referring to static files (where they will be served from)
+STATIC_URL = '/static/'
+
+
+# Simplified static file serving.
+# https://warehouse.python.org/project/whitenoise/
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+
+
+# Heroku: Update database configuration from $DATABASE_URL.
+
+import dj_database_url
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn DjangoBootcampSample.wsgi --log-file -
